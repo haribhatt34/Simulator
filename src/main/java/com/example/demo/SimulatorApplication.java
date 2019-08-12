3de2116fe617d7fa8d83a92ed68a8f57757053c1
ajayarora3899 <ajayarora3899@gmail.com>
2019-08-12 11:14:04 +0530

    final Simulator commit

--- src/main/java/com/example/demo/SimulatorApplication.java
+++ src/main/java/com/example/demo/SimulatorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimulatorGitApplication {
+public class SimulatorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimulatorGitApplication.class, args);
+		SpringApplication.run(SimulatorApplication.class, args);
 	}
 
 }
